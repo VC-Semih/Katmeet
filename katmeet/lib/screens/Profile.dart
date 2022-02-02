8ce30e4b849a33600947ff4d3a65c9af6880d32a
BAKHSHI-Faisal <afaisal_bakhshi@live.com>
2022-02-02 13:38:58 +0100

    fixed: style profile

--- katmeet/lib/screens/Profile.dart
+++ katmeet/lib/screens/Profile.dart
@@ -11,46 +11,214 @@ class _DashboardScreenState extends State<DashboardScreen> {
   AuthUser _user;
 
   @override
+  void initState() {
+    super.initState();
+    Amplify.Auth.getCurrentUser().then((user) {
+      setState(() {
+        _user = user;
+      });
+    }).catchError((error) {
+      print((error as AuthException).message);
+
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Dashboard'),
-        actions: [
-          MaterialButton(
-            onPressed: () {
-              Amplify.Auth.signOut().then((_) {
-                Navigator.pushReplacementNamed(context, '/');
-              });
-            },
-            child: Icon(
-              Icons.logout,
-              color: Colors.white,
-            ),
-          )
-        ],
-      ),
-      body: Container(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (_user == null)
-                Text(
-                  'Loading...',
-                )
-              else ...[
-                Text(
-                  'Meowww 😺',
-                  style: Theme.of(context).textTheme.headline2,
+      body: Column(
+        children: <Widget>[
+          Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [Colors.grey[200], Colors.grey[200]]
+                  )
+              ),
+              child: Container(
+                width: double.infinity,
+                height: 350.0,
+                child: Center(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      SizedBox(
+                        height: 80.0,
+                      ),
+                      CircleAvatar(backgroundImage: NetworkImage('https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&rect=37%2C29%2C4955%2C3293&q=45&auto=format&w=926&fit=clip'),radius: 50.0,),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        _user.username,
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Card(
+                        margin: EdgeInsets.symmetric(horizontal: 20.0,vertical: 5.0),
+                        clipBehavior: Clip.antiAlias,
+                        color: Colors.white,
+                        elevation: 5.0,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 8.0,vertical: 22.0),
+                          child: Row(
+                            children: <Widget>[
+                              Expanded(
+                                child: Column(
+
+                                  children: <Widget>[
+                                    Text(
+                                      "Posts",
+                                      style: TextStyle(
+                                        color: Colors.blueGrey,
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "1",
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.blueGrey,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+
+                                  children: <Widget>[
+                                    Text(
+                                      "Adopted",
+                                      style: TextStyle(
+                                        color: Colors.blueGrey,
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "2",
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.blueGrey,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Expanded(
+                                child: Column(
+
+                                  children: <Widget>[
+                                    Text(
+                                      "Given",
+                                      style: TextStyle(
+                                        color: Colors.blueGrey,
+                                        fontSize: 22.0,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(
+                                      height: 5.0,
+                                    ),
+                                    Text(
+                                      "2",
+                                      style: TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.blueGrey,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
                 ),
-                Text(_user.username),
-                SizedBox(height: 10),
-                Text(_user.userId),
-                SizedBox(height: 10),
-              ],
-            ],
+              )
           ),
-        ),
+          Container(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30.0,horizontal: 16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Bio:",
+                    style: TextStyle(
+                        color: Colors.blueGrey,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 28.0
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Text('kitty Cat.\n'
+                      'if you need any mobile app for your company then contact me for more informations',
+                    style: TextStyle(
+                      fontSize: 22.0,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.black,
+                      letterSpacing: 2.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            width: 300.00,
+
+            child: RaisedButton(
+                onPressed: (){},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(80.0)
+                ),
+                elevation: 0.0,
+                padding: EdgeInsets.all(0.0),
+                child: Ink(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.centerRight,
+                        end: Alignment.centerLeft,
+                        colors: [Colors.blueGrey,Colors.blueAccent]
+                    ),
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  child: Container(
+                    constraints: BoxConstraints(maxWidth: 300.0, minHeight: 50.0),
+                    alignment: Alignment.center,
+                    child: Text("Edit",
+                      style: TextStyle(color: Colors.white, fontSize: 26.0, fontWeight:FontWeight.w300),
+                    ),
+                  ),
+                )
+            ),
+          ),
+        ],
       ),
     );
   }
