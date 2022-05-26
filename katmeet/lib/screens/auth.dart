f6bdb0d1698229d20a88bc55d9deebbc20f66058
BAKHSHI-Faisal <afaisal_bakhshi@live.com>
2022-05-26 11:35:04 +0200

    fixed icon login page

--- katmeet/lib/screens/auth.dart
+++ katmeet/lib/screens/auth.dart
@@ -15,7 +15,8 @@ class Authenticator extends StatelessWidget {
               title: Text("KatMeet"),
               leading: Padding(
                   padding: EdgeInsets.fromLTRB(15, 5, 0, 5),
-                  child: Image.asset('assets/kat.png')),
+              child: CircleAvatar(backgroundImage: NetworkImage('https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&rect=37%2C29%2C4955%2C3293&q=45&auto=format&w=926&fit=clip'),),
+              )                  ,
               bottom: TabBar(
                 tabs: [
                   Tab(icon: Icon(Icons.person_add)),
